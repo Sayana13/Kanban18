228ea3b1a6bf43606c70f95084c10f1e8d694557
Sayana13 <domshoevas@gmail.com>
2021-03-20 14:00:14 -0700

    Updated Kanban

--- src/Card.js
+++ src/Card.js
@@ -1,36 +1,37 @@
 import UpdateCardModal from "./UpdateCardModal";
 
-function Card (props) {
+function Card(props) {
 
-    const {description, name, priority, status} = props.card;
+    const {description, name, priority, status} = props.cards;
 
     return (
-        <div className="card" >
+        <div className="card">
             <div className="card-body">
                 <h5 className="card-title">{name}</h5>
                 <h6 className="card-subtitle mb-2 text-muted">{status}</h6>
                 <p className="card-text">{description}</p>
                 <p className="card-text">Priority: {priority}</p>
+                <button type="button" className="btn btn-outline-primary"
+                        disabled={props.cards.status === props.columns[0]}
+                        onClick={() => props.nextStatus(props.cards, 'left')}>←
+                </button>
+                <button type="button" className="btn btn-outline-primary"
+                        disabled={props.cards.status === props.columns[props.columns.length - 1]}
+                        onClick={() => props.nextStatus(props.cards, 'right')}>→
+                </button>
                 <button type="button" className="btn btn-outline-primary">↑</button>
                 <button type="button" className="btn btn-outline-primary">↓</button>
-                <button type="button" className="btn btn-outline-primary"
-                        disabled={props.card.status===props.columns[0]}
-                        onClick={()=>props.nextStatus(props.card, 'left')}
-                >←</button>
-                <button type="button" className="btn btn-outline-primary"
-                        disabled={props.card.status=== props.columns[props.columns.length-1]}
-                        onClick={()=>props.nextStatus(props.card, 'right')}>→</button>
                 <UpdateCardModal
                     columns={props.columns}
                     priority={props.priority}
-                    card={props.card}
+                    card={props.cards}
                 />
                 <button type="button" className="btn btn-outline-danger">Delete</button>
             </div>
         </div>
-
     );
 }
+
  export default Card;
 
 
--- src/Column.js
+++ src/Column.js
@@ -1,21 +1,21 @@
 import Card from "./Card";
 
 function Column(props) {
-
     return (
         <div className="col text-capitalize">
-
             <h3>{props.status.title}</h3>
-            {props.card.filter(el => props.status.status === el.status)
-                .map(el => <Card card={el}
-                                 key={el._id}
-                                 nextStatus={props.nextStatus}
-                                 columns={props.columns}
-                                 priority={props.property}
+            {props.cards
+                .filter(el => props.status.status === el.status)
+                .map(el => <Card
+                    cards={el}
+                    key={el._id}
+                    nextStatus={props.nextStatus}
+                    columns={props.columns}
+                    priority={props.priority}
                 />)}
         </div>
     );
-};
+}
 
 export default Column;
 
